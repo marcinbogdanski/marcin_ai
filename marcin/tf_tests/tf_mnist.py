eddbdbee778ffd50a04ba4788046c891a4b4725d
Marcin Bogdanski <marcin.bogdanski@dronex.co.uk>
2017-10-05 17:41:15 +0100

    Add mnist data loading and conversin

--- marcin/tf_tests/tf_mnist.py
+++ marcin/tf_tests/tf_mnist.py
@@ -3,16 +3,57 @@
 import tensorflow as tf
 import sklearn
 import sys, os
-
-#parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#os.sys.path.insert(0,parentdir) 
-
+import numpy as np
+import matplotlib.pyplot as plt
+import dxlib.feeders as dxfd
 
 import pdb
 
-import dxlib.feeders as dxfd
+def load_mnist(test=False):
+    from sklearn.datasets import fetch_mldata
+
+    # loads data into ~/scikit_learn_data/
+    mnist = fetch_mldata('MNIST original')
+
+    # Convert to float32, range 0..1; shape [70000, 784]
+    data = mnist.data.astype(np.float32) / 255.0
+
+    # One-hot encode labels, shape [70000, 10]
+    eye = np.eye(10, dtype=np.int32)
+    tar = mnist.target.astype(np.int32)
+    targets = eye[tar]
+
+    train = dxfd.Feeder(data[0:55000], targets[0:55000])
+    valid = dxfd.Feeder(data[55000:65000], targets[55000:65000])
+    test  = dxfd.Feeder(data[65000:70000], targets[65000:70000])
+
+    return train, valid, test
+
+def show_pics(feeder, start, num, cols=10):
+    rows = np.ceil(num/cols)
+
+    fig = plt.figure()
+    for n in range(num):
+        temp = feeder.features[start + n]
+        label  = feeder.labels[start + n]
+        data = temp.reshape([28, 28])
+        a = fig.add_subplot(cols, rows, n+1)
+        #a.set_title('label: ' + str(label))
+        plt.imshow(data, cmap='gray')
+    plt.show()
+
+
+train, valid, test = load_mnist()
+
+show_pics(train, start=0, num=25, cols=5)
+
+exit(0)
+
+
 
 ff = dxfd.Feeder()
+
+pdb.set_trace()
 
 exit(0)
 
